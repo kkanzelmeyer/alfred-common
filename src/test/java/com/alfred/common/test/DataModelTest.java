4e0ec847d7c8407adfd8695d02f844d03fafe8a0
kevin <kanzelm4@gmail.com>
2015-10-15 23:26:20 -0500

    minor update to a state test

--- src/test/java/com/alfred/common/test/DataModelTest.java
+++ src/test/java/com/alfred/common/test/DataModelTest.java
@@ -6,7 +6,6 @@ import org.junit.Test;
 
 import com.alfred.common.datamodel.StateDevice;
 import com.alfred.common.datamodel.StateDeviceManager;
-import com.alfred.common.handlers.StateDeviceHandler;
 import com.alfred.common.messages.StateDeviceProtos.StateDeviceMessage.State;
 import com.alfred.common.messages.StateDeviceProtos.StateDeviceMessage.Type;
 
@@ -61,67 +60,9 @@ public class DataModelTest {
         }
     }
 
-    /**
-     * Test to verify handler functionality
-     */
     @Test
-    public void testDeviceHandler() {
-
-        // Add a handler
-        StateDeviceManager.addDeviceHandler(new DoorbellStateHandler());
-        
-        // Create test device 1
-        String id1 = "doorbell1";
-        StateDevice device = new StateDevice.Builder()
-                            .setId(id1)
-                            .setType(Type.DOORBELL)
-                            .setName("Front Door")
-                            .setState(State.INACTIVE)
-                            .build();
-
-
-        // create test device two
-        String id2 = "doorbell2";
-        StateDevice device2 = new StateDevice.Builder()
-                            .setId(id2)
-                            .setType(Type.DOORBELL)
-                            .setName("Side Door")
-                            .setState(State.INACTIVE)
-                            .build();
-
-
-        // Add the new devices
-        StateDeviceManager.addStateDevice(device);
-        StateDeviceManager.addStateDevice(device2);
-
-        // Update the device
-        State newState = State.ACTIVE;
-        StateDeviceManager.updateStateDevice(id1, newState);
-        
-        // Test case
-        StateDevice clone = StateDeviceManager.getDevice(id1);
-        assertEquals(newState, clone.getState());
-    }
-    
-    
-    private class DoorbellStateHandler implements StateDeviceHandler {
-
-        public void onAddDevice(StateDevice device) {
-            if(device.getType() == Type.DOORBELL) {
-                System.out.println("\nDevice added:" + device.toString());
-            }
-        }
-
-        public void onUpdateDevice(StateDevice device) {
-            if(device.getType() == Type.DOORBELL) {
-                System.out.println("\nDevice updated:" + device.toString());
-            }
-        }
-
-        public void onRemoveDevice(StateDevice device) {
-            // TODO Auto-generated method stub
-            
-        }
-        
+    public void testProtobufEnum() {
+        Type type = Type.valueOf("DOORBELL");
+        assertEquals(Type.DOORBELL, type);
     }
 }
